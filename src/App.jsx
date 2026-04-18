20761d037d2f4eebeaa604da3ec0bfb3f75aa37d
Gastón Berhau <gastonn.okk@gmail.com>
2026-04-18 16:48:35 -0300

    Inicializacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Details from "./pages/Details.jsx";
+import Favorites from "./pages/Favorites.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
